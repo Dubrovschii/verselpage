3e490b14cf0522d5ece2fc9615f19a9659e964c7
Dubrovschii <dubrovschii@gmail.com>
2025-03-07 11:11:02 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <div class="malevich"></div>
+  </div>
+</template>
+
+<style scoped>
+.malevich {
+  background: #181818;
+  width: 200px;
+  height: 200px;
+}
+</style>
